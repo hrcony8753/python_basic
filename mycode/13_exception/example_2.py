55e0656c298bd15f8e844f1145c24e824ca3d9a3
Lenovo <hrcony8753>
2021-01-05 11:47:47 +0900

    코드 수정

--- mycode/13_exception/example_2.py
+++ mycode/13_exception/example_2.py
@@ -1,6 +1,12 @@
+'''
+try~except문 없다면 바로 실행 오류
+'''
+
 for i in range(10):
     try:
         print(i, 10 // i)
     except ZeroDivisionError as err:
         print(err)
         print("Not divided by 0")
+    finally:
+        print("END~~")
